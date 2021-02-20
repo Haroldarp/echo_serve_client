c4fd5ef44f77ab164e79dccb01db9f03a3e4617c
Harold Rodriguez <62029812+Haroldarp@users.noreply.github.com>
2021-02-20 12:29:08 -0400

    Add sent message and print response

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,14 @@ fn main() {
 
     loop{
         let mut message = String::new();
-        // let mut buf : Vec<u8> = Vec::new();
+        let mut buf : Vec<u8> = Vec::new();
         io::stdin().read_line(&mut message).expect("No se pudo leer el mesaje");
         stream.write(message.as_bytes()).expect("No se pudo enviar el mensaje");
+
+        let mut reader = BufReader::new(&stream);
+        reader.read_until(b'\n', &mut buf).expect("No se pudo leer la respuesta");
+        println!("{}", str::from_utf8(&buf).expect("No se pudieron castear los bytes"));
+    
     }
     
 }
